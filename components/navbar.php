6904d655abfc70c02dd1a174a4ac769e38b4aa02
Rfkinrikhwan <rifkinurikhwan9@gmail.com>
2025-01-22 01:31:49 +0700

    feat: Add Feature Login

--- components/navbar.php
+++ components/navbar.php
@@ -1,3 +1,8 @@
+<?php
+$userName = isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest';
+$userEmail = isset($_SESSION['email']) ? $_SESSION['email'] : 'guest@example.com';
+?>
+
 <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
     <div class="px-3 py-3 lg:px-5 lg:pl-3">
         <div class="flex items-center justify-between">
@@ -8,8 +13,8 @@
                         <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                     </svg>
                 </button>
-                <a href="#" class="flex ms-2 md:me-24">
-                    <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Manajemen Kos</span>
+                <a href="index.php" class="flex ms-2 md:me-24">
+                    <h2 class="text-3xl font-bold text-center text-white">Kos <span class="bg-orange-400 px-3 rounded-md text-black">Hub</span></h2>
                 </a>
             </div>
             <div class="flex items-center">
@@ -23,15 +28,15 @@
                     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
                         <div class="px-4 py-3" role="none">
                             <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                Neil Sims
+                                <?php echo htmlspecialchars($userName); ?>
                             </p>
                             <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                neil.sims@flowbite.com
+                                <?php echo htmlspecialchars($userEmail); ?>
                             </p>
                         </div>
                         <ul class="py-1" role="none">
                             <li>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                                <a href="../config/logout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
                             </li>
                         </ul>
                     </div>
--- components/sidebar.php
+++ components/sidebar.php
@@ -1,37 +1,37 @@
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
-            <li>
+            <li <?php echo ($_SERVER['PHP_SELF'] == '/index.php') ? 'class="bg-gray-100 dark:bg-gray-700 rounded-lg"' : ''; ?>>
                 <a href="index.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <i class="fa-solid fa-house"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Kamar</span>
                 </a>
             </li>
-            <li>
+            <li <?php echo ($_SERVER['PHP_SELF'] == '/penyewa.php') ? 'class="bg-gray-100 dark:bg-gray-700 rounded-lg"' : ''; ?>>
                 <a href="penyewa.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <i class="fa-solid fa-users"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Penyewa</span>
                 </a>
             </li>
-            <li>
+            <li <?php echo ($_SERVER['PHP_SELF'] == '/reservasi.php') ? 'class="bg-gray-100 dark:bg-gray-700 rounded-lg"' : ''; ?>>
                 <a href="reservasi.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <i class="fa-solid fa-book"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Reservasi</span>
                 </a>
             </li>
-            <li>
+            <li <?php echo ($_SERVER['PHP_SELF'] == '/laporan.php') ? 'class="bg-gray-100 dark:bg-gray-700 rounded-lg"' : ''; ?>>
                 <a href="laporan.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <i class="fa-solid fa-book"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Laporan</span>
                 </a>
             </li>
-            <li>
+            <li <?php echo ($_SERVER['PHP_SELF'] == '/pembayaran.php') ? 'class="bg-gray-100 dark:bg-gray-700 rounded-lg"' : ''; ?>>
                 <a href="pembayaran.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <i class="fa-solid fa-money-bill"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Pembayaran</span>
                 </a>
             </li>
-            <li>
+            <li <?php echo ($_SERVER['PHP_SELF'] == '/admin.php') ? 'class="bg-gray-100 dark:bg-gray-700 rounded-lg"' : ''; ?>>
                 <a href="admin.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <i class="fa-solid fa-user"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Admin</span>
